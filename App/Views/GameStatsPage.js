d7317a4f70487910ecbcd55e7b9607c9e8650d36
James <jbkuczma@buffalo.edu>
2016-06-17 12:07:22 -0400

    added scroll tab bar

--- App/Views/GameStatsPage.js
+++ App/Views/GameStatsPage.js
@@ -10,7 +10,12 @@ import {
   TouchableHighlight,
 } from 'react-native';
 
+import ScrollableTabView, {
+    DefaultTabBar,
+} from 'react-native-scrollable-tab-view';
+
 import TeamMap from '../Utilities/TeamMap';
+import GameStatsTeam from './GameStatsTeam';
 
 class GameStatsPage extends React.Component {
 
@@ -18,31 +23,38 @@ class GameStatsPage extends React.Component {
         super(props);
     }
 
-    componentWillMount(){
-        this.getGameStats();
-    }
-
-    getGameStats(){
-        var game = this.props.game;
-        var date = game.date;
-        var gameID = game.id;
-        var url = 'http://data.nba.com/data/10s/json/cms/noseason/game/' + date + '/' + gameID + '/boxscore.json';
-        fetch(url)
-        .then((response) => response.json())
-        .then((jsonResponse) => {
-            console.log(jsonResponse);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }
+    // componentWillMount(){
+    //     this.getGameStats();
+    // }
+    //
+    // getGameStats(){
+    //     var game = this.props.game;
+    //     var date = game.date;
+    //     var gameID = game.id;
+    //     var url = 'http://data.nba.com/data/10s/json/cms/noseason/game/' + date + '/' + gameID + '/boxscore.json';
+    //     fetch(url)
+    //     .then((response) => response.json())
+    //     .then((jsonResponse) => {
+    //         console.log(jsonResponse);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // }
 
     render(){
         var game = this.props.game;
-        // console.log(game);
         return(
             <View style={styles.main}>
-                <Text> hi how are ya? </Text>
+                <ScrollableTabView
+                    renderTabBar={() => <DefaultTabBar style={{marginTop: 62, backgroundColor: '#0288D1', borderColor: '#0288D1'}}/>}
+                    tabBarActiveTextColor='#FFFFFF'
+                    tabBarInactiveTextColor='#e5e5e5'
+                    tabBarUnderlineColor='#b3dbf1'
+                >
+                    <GameStatsTeam tabLabel='Teams' game={game}/>
+                    <Text tabLabel='Players'> there </Text>
+                </ScrollableTabView>
             </View>
         )
     }
@@ -51,8 +63,8 @@ class GameStatsPage extends React.Component {
 var styles = StyleSheet.create({
     main: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // justifyContent: 'center',
+        // alignItems: 'center',
         backgroundColor: '#FCFCFC',
     }
 });
