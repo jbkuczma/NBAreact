ac00cb00caab723aee25b3effd23d1a6fb0303a1
James <jbkuczma@buffalo.edu>
2016-06-17 09:13:04 -0400

    retrieved game info stats for team AND players

--- App/Views/GameStatsPage.js
+++ App/Views/GameStatsPage.js
@@ -1,3 +1,5 @@
+//http://data.nba.com/data/10s/json/cms/noseason/game/${gameDate}/${gameId}/boxscore.json
+
 import React, {Component,} from 'react';
 import {
   AppRegistry,
@@ -16,8 +18,28 @@ class GameStatsPage extends React.Component {
         super(props);
     }
 
+    componentWillMount(){
+        this.getGameStats();
+    }
+
+    getGameStats(){
+        var game = this.props.game;
+        var date = game.date;
+        var gameID = game.id;
+        var url = 'http://data.nba.com/data/10s/json/cms/noseason/game/' + date + '/' + gameID + '/boxscore.json';
+        fetch(url)
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+            console.log(jsonResponse);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
     render(){
         var game = this.props.game;
+        // console.log(game);
         return(
             <View style={styles.main}>
                 <Text> hi how are ya? </Text>
