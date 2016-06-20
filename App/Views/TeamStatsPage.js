bc393b740fee84bb34cd594d44e0ae0e8b0ef0f4
James <jbkuczma@buffalo.edu>
2016-06-20 09:13:26 -0400

    team header with team stats is complete

--- App/Views/TeamStatsPage.js
+++ App/Views/TeamStatsPage.js
@@ -8,19 +8,29 @@ import {
   Text,
   View,
   StyleSheet,
-  Image
+  Image,
+  ListView,
+  Dimensions
 } from 'react-native';
 
 import TeamMap from '../Utilities/TeamMap';
+import PlayerCell from './PlayerCell';
+
+let windowHeight = Dimensions.get('window').height;
 
 class GameStatsTeam extends React.Component {
 
   constructor(props){
     super(props);
+    var ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
     this.state = {
       loaded: false,
       teamStatsRecord: [],
-      teamStatsLeague: []
+      teamStatsLeague: [],
+      playerStats: [],
+      dataSource: ds.cloneWithRows([]),
     }
   }
 
@@ -43,21 +53,40 @@ class GameStatsTeam extends React.Component {
 
   componentWillMount(){
     this.getTeamStats();
+    this.getPlayers();
   }
 
   getTeamStats(){
     var team = this.props.team;
     var teamID = TeamMap[team].id;
-    var season = '2015-16';
+    var season = '2015-16'; // IMPORTANT
     var url = 'http://stats.nba.com/stats/teaminfocommon?LeagueID=00&SeasonType=Regular+Season&TeamID=' + teamID + '&season=' + season;
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      this.setState({
+        loaded: true,
+        teamStatsRecord: jsonResponse.resultSets[0].rowSet,
+        teamStatsLeague: jsonResponse.resultSets[1].rowSet
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  getPlayers(){
+    var season = '2015-16'; // IMPORTANT
+    var team = this.props.team;
+    var teamID = TeamMap[team].id;
+    var url = 'http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season=' + season + '&SeasonSegment=&SeasonType=Regular+Season&TeamID=' + teamID + '&VsConference=&VsDivision';
     fetch(url)
     .then((response) => response.json())
     .then((jsonResponse) => {
       console.log(jsonResponse);
       this.setState({
-        loaded: true,
-        teamStatsRecord: jsonResponse.resultSets[0].rowSet,
-        teamStatsLeague: jsonResponse.resultSets[1].rowSet
+        playerStats: jsonResponse.resultSets[1].rowSet,
+        dataSource: this.state.dataSource.cloneWithRows(jsonResponse.resultSets[1].rowSet)
       });
     })
     .catch((error) => {
@@ -70,8 +99,8 @@ class GameStatsTeam extends React.Component {
     if (!this.state.loaded){
       return <View><Text> Fetching data </Text></View>;
     }
-    console.log(this.state.teamStatsRecord);
-    console.log(this.state.teamStatsLeague);
+    // console.log(this.state.teamStatsRecord);
+    // console.log(this.state.teamStatsLeague);
     return (
       <View style={styles.body}>
         <View style={[styles.header, {backgroundColor: TeamMap[this.state.teamStatsRecord[0][4].toLowerCase()].color}]}>
@@ -86,21 +115,44 @@ class GameStatsTeam extends React.Component {
             />
           </View>
           <View style={styles.rankings1}>
-            <Text> Wins: {this.state.teamStatsRecord[0][8]} </Text>
-            <Text> Losses: {this.state.teamStatsRecord[0][9]} </Text>
-            <Text> {this.rankingSuffix(this.state.teamStatsRecord[0][12])} in the {this.state.teamStatsRecord[0][5]} </Text>
-            <Text> {this.rankingSuffix(this.state.teamStatsRecord[0][11])} in the {this.state.teamStatsRecord[0][6]} </Text>
-            </View>
+            <Text style={styles.leagueRankingsText}> Wins: {this.state.teamStatsRecord[0][8]} </Text>
+            <Text style={styles.leagueRankingsText}> Losses: {this.state.teamStatsRecord[0][9]} </Text>
+            <Text style={styles.leagueRankingsText}> {this.rankingSuffix(this.state.teamStatsRecord[0][12])} in the {this.state.teamStatsRecord[0][5]} </Text>
+            <Text style={styles.leagueRankingsText}> {this.rankingSuffix(this.state.teamStatsRecord[0][11])} in the {this.state.teamStatsRecord[0][6]} </Text>
           </View>
-          <View style={styles.secondHeader}>
-            <Text> PPG {this.state.teamStatsLeague[0][4]} ({this.state.teamStatsLeague[0][5]}) </Text>
-            <Text> OPP PPG {this.state.teamStatsLeague[0][10]} ({this.state.teamStatsLeague[0][9]}) </Text>
-            <Text> RPG {this.state.teamStatsLeague[0][6]} ({this.state.teamStatsLeague[0][5]}) </Text>
-            <Text> APG {this.state.teamStatsLeague[0][8]} ({this.state.teamStatsLeague[0][7]}) </Text>
-          </View>
-        <View style={{flex: 1}}>
-          <Text> add listview of players </Text>
         </View>
+        <View style={styles.teamStatsRankings}>
+          <View style={styles.rankingItem}>
+            <Text style={styles.itemLabel}> PPG </Text>
+            <Text style={styles.itemData}> {this.state.teamStatsLeague[0][4]} </Text>
+            <Text style={styles.itemData}> ({this.rankingSuffix(this.state.teamStatsLeague[0][3])}) </Text>
+          </View>
+          <View style={styles.rankingItem}>
+            <Text style={styles.itemLabel}> OPP PPG </Text>
+            <Text style={styles.itemData}> {this.state.teamStatsLeague[0][10]} </Text>
+            <Text style={styles.itemData}> ({this.rankingSuffix(this.state.teamStatsLeague[0][9])}) </Text>
+          </View>
+          <View style={styles.rankingItem}>
+            <Text style={styles.itemLabel}> RPG </Text>
+            <Text style={styles.itemData}> {this.state.teamStatsLeague[0][6]} </Text>
+            <Text style={styles.itemData}> ({this.rankingSuffix(this.state.teamStatsLeague[0][5])}) </Text>
+          </View>
+          <View style={styles.rankingItem}>
+            <Text style={styles.itemLabel}> APG </Text>
+            <Text style={styles.itemData}> {this.state.teamStatsLeague[0][8]} </Text>
+            <Text style={styles.itemData}> ({this.rankingSuffix(this.state.teamStatsLeague[0][7])}) </Text>
+          </View>
+        </View>
+        <ListView
+          style={{backgroundColor: '#FCFCFC', height: windowHeight, marginTop: 15}}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData, sectionID, rowID) =>
+            <PlayerCell
+              player={rowData}
+            //   onPress={() => this.props.goToGameStats(rowData)}
+            />
+          }
+        />
       </View>
     )
   }
@@ -110,7 +162,8 @@ var styles = StyleSheet.create({
   body: {
     // flex: 2,
     // marginTop: 62
-    flexDirection: 'column'
+    flexDirection: 'column',
+    backgroundColor: '#FCFCFC'
   },
   header: {
       marginTop: 62,
@@ -132,9 +185,38 @@ var styles = StyleSheet.create({
       justifyContent: 'center',
       marginRight: 15
   },
+  leagueRankingsText: {
+    fontWeight: '400',
+    fontSize: 12,
+    color: '#FCFCFC'
+  },
   secondHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  teamStatsRankings: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 65
+  },
+  rankingItem: {
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  itemLabel: {
+    color: '#4a5669',
+    fontSize: 12,
+    fontWeight: '200'
+  },
+  itemData: {
+    color: '#404a5a',
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginLeft: 2,
+    position: 'relative',
+    top: 2
   }
 });
 
