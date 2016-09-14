4b7d5c6e7e15326107f0d10b87f78d3903d50daa
James <jbkuczma@buffalo.edu>
2016-09-14 10:36:15 -0400

    beginning stages of NBAreact for the web. Currently have webpack set up

---
+++ WebApp/src/app/jsx/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return(
+        <div>
+            <h1> NBAreactWeb</h1>
+        </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'));
---
+++ WebApp/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/public'); //build file output
+var APP_DIR = path.resolve(__dirname, 'src/app'); //app codebase
+
+var config = {
+  entry: APP_DIR + '/jsx/app.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
