4a8c75b96f0d6813fdab91e7b68fc797ff03d37c
James <jbkuczma@buffalo.edu>
2018-02-11 13:43:38 -0500

    add editorconfig file

--- App/components/ScoresScreen/ScoresScreen.js
+++ App/components/ScoresScreen/ScoresScreen.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class ScoresScreen extends Component<Props> {
-    render() {
-        return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text> Scores </Text>
-        </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> Scores </Text>
+      </View>
+    );
+  }
 }
--- App/components/StandingsScreen/StandingsScreen.js
+++ App/components/StandingsScreen/StandingsScreen.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class StandingsScreen extends Component<Props> {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text> Standings </Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> Standings </Text>
+      </View>
+    );
+  }
 }
