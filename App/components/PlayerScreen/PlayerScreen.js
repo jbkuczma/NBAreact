a304bc0b3647dfa56fb9ed5618303f1711ca2fdc
James <jbkuczma@buffalo.edu>
2018-03-10 11:25:57 -0500

    changed layout of player game log display. old version is currently commented out

--- App/components/PlayerScreen/PlayerScreen.js
+++ App/components/PlayerScreen/PlayerScreen.js
@@ -71,16 +71,42 @@ class PlayerScreen extends Component<Props> {
     const matchup = game.matchup.match(/(@|vs\.)\s[a-zA-Z]+/)[0].replace('.', '')
 
     return (
-      <TouchableOpacity style={{ flexDirection: 'row', marginLeft: 10, marginRight: 10, height: 100 }}>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-          <Text> </Text>
-          <Text style={styles.textSecondary}> {game.wl} </Text>
-        </View>
-        <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginLeft: 10, marginRight: 10  }}>
-          <Text style={styles.textSecondary}> {game.game_date} </Text>
-          <Text style={styles.textSecondary}> {matchup} </Text>
+      // <TouchableOpacity style={{ flexDirection: 'row', marginLeft: 10, marginRight: 10, height: 100 }}>
+      //   <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      //     <Text> </Text>
+      //     <Text style={styles.textSecondary}> {game.wl} </Text>
+      //   </View>
+      //   <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginLeft: 10, marginRight: 10  }}>
+      //     <Text style={styles.textSecondary}> {game.game_date} </Text>
+      //     <Text style={styles.textSecondary}> {matchup} </Text>
+      //   </View>
+      //   <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 5, marginRight: 5 }}>
+      //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      //       <Text style={styles.textPrimary}>
+      //         {game.pts} <Text style={styles.textSecondary}>pts</Text>
+      //       </Text>
+      //     </View>
+      //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      //       <Text style={styles.textPrimary}>
+      //         {game.reb} <Text style={styles.textSecondary}>reb</Text>
+      //       </Text>
+      //     </View>
+      //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      //       <Text style={styles.textPrimary}>
+      //         {game.ast} <Text style={styles.textSecondary}>ast</Text>
+      //       </Text>
+      //     </View>
+      //   </View>
+      // </TouchableOpacity>
+      <TouchableOpacity style={{ flexDirection: 'column', marginLeft: 10, marginRight: 10, height: 100, borderBottomColor: '#444444', borderBottomWidth: 1 }}>
+        {/* <View style={{ flex: 1, justifyContent: 'center', justifyContent: 'center', alignItems: 'center'  }}> */}
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text style={[styles.textSecondary, { textAlign: 'left'}]}> {game.game_date} </Text>
         </View>
         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 5, marginRight: 5 }}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={styles.textSecondary}> {game.wl} {matchup} </Text>
+          </View>
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text style={styles.textPrimary}>
               {game.pts} <Text style={styles.textSecondary}>pts</Text>
