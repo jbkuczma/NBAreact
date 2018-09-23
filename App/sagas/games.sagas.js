fde71ae67417a2ac6fb2fa8c7afc667f0986b2b3
James <jbkuczma@buffalo.edu>
2018-09-23 11:16:49 -0400

    begin move of fetching team game stats in sagas

--- App/sagas/games.sagas.js
+++ App/sagas/games.sagas.js
@@ -1,7 +1,10 @@
-import { put, takeLatest, select } from 'redux-saga/effects'
+import { put, takeLatest, select, call } from 'redux-saga/effects'
 import NBA from '../utils/nba'
 
 this.nba = new NBA()
+
+const getDate = (state) => state.date.date;
+const getSeason = (state) => state.date.season;
 
 function* getGames(payload) {
   try {
@@ -15,6 +18,56 @@ function* getGames(payload) {
   }
 }
 
+function* getGameStats(payload) {
+  try {
+    const selectedGameData = payload.selectedGame;
+    const { gameID, homeTeam, awayTeam } = selectedGameData;
+    const date = yield select(getDate)
+    const season = yield select(getSeason)
+
+    const teamStatsResponseData = yield call(getTeamStatsForGame, gameID);
+    const boxscoreResponseData = yield this.nba.getBoxscore(gameID, date)
+    const playByPlayResponseData = yield this.nba.getPlayByPlay(gameID, season)
+    const teamStatsData = teamStatsResponseData
+    const boxscoreData = boxscoreResponseData.stats || {};
+    const playByPlayData = playByPlayResponseData.g.pd // todo: clean data
+
+    yield put({
+      type: 'GET_GAME_STATS_SUCCESS',
+      data: {
+        teamStatsData,
+        boxscoreData,
+        playByPlayData
+      }
+    })
+  } catch (error) {
+    yield put({ type: 'GET_GAME_STATS_FAILURE', error: 'Error fetching game stats' })
+  }
+}
+
+function* getTeamStatsForGame(gameID) {
+  try {
+    const date = yield select(getDate)
+
+    let teamBoxscoreData = yield this.nba.getBoxscore(gameID, date)
+    const leadTracker = yield this.nba.getLeadTrackerForGame(gameID, date)
+    const miniBoxscoreData = yield this.nba.getMiniBoxscore(gameID, date)
+
+    teamBoxscoreData = teamBoxscoreData.stats ? teamBoxscoreData.stats : null
+    // also has `basicGameData` and `previousMAtchup`
+
+    return {
+      teamBoxscoreData,
+      leadTracker,
+      miniBoxscoreData
+    }
+
+  } catch (error) {
+    yield put({ type: 'GET_TEAMS_STATS_FOR_GAME_FAILURE' })
+  }
+}
+
 export const gamesSagas = [
-  takeLatest('GET_GAMES_REQUEST', getGames)
+  takeLatest('GET_GAMES_REQUEST', getGames),
+  takeLatest('SELECT_GAME', getGameStats)
 ]
